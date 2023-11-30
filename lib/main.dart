d54f48c476210f186dccaa4efa9d108f08b238bd
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2023-11-30 22:10:12 +0300

    feat: lesson 26

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://c8.alamy.com/comp/2NHN89F/flutter-dart-code-in-android-studio-in-a-black-background-2NHN89F.jpg'),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
